278f542c0630b2dab094ade877505f096527f5f6
naiarb <braianmosqueira92@hotmail.com>
2026-09-13 20:21:35 -0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,8 @@
 window.AulaGramConfig = Object.freeze({
-  VERSION: '8.0.0',
+  VERSION: '8.1.0',
   // URL del despliegue /exec de Google Apps Script.
   BACKEND_URL: 'https://script.google.com/macros/s/AKfycbyWTSIFsOshhVtD3YTU0s3hqIealk_oGtfJrW2W6R555YwY2c8SlhUy1QuxRVVeFXL6oA/exec',
   FRONTEND_URL: 'https://braianm-dev.github.io/aulagram/',
-  RPC_TIMEOUT_MS: 26000,
-  BRIDGE_READY_TIMEOUT_MS: 15000
+  RPC_TIMEOUT_MS: 30000,
+  BRIDGE_READY_TIMEOUT_MS: 12000
 });
