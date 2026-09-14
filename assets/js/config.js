7c7d39cf37b2c397c8ff4066773adaf73e06a31f
naiarb <braianmosqueira92@hotmail.com>
2026-09-14 15:51:39 -0300

    Bump AulaGram frontend to v8.6 lightweight mode

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,5 @@
 window.AulaGramConfig = Object.freeze({
-  VERSION: '8.5.0',
-  // URL del despliegue /exec de Google Apps Script.
+  VERSION: '8.6.0',
   BACKEND_URL: 'https://script.google.com/macros/s/AKfycbyWTSIFsOshhVtD3YTU0s3hqIealk_oGtfJrW2W6R555YwY2c8SlhUy1QuxRVVeFXL6oA/exec',
   FRONTEND_URL: 'https://braianm-dev.github.io/aulagram/',
   RPC_TIMEOUT_MS: 35000,
