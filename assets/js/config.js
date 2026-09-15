17857948e0410648ba31c45b61cf04197c05dd3e
naiarb <braianmosqueira92@hotmail.com>
2026-09-15 00:03:07 -0300

    Bump AulaGram frontend metadata to v8.6.1

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.AulaGramConfig = Object.freeze({
-  VERSION: '8.6.0',
+  VERSION: '8.6.1',
   BACKEND_URL: 'https://script.google.com/macros/s/AKfycbyWTSIFsOshhVtD3YTU0s3hqIealk_oGtfJrW2W6R555YwY2c8SlhUy1QuxRVVeFXL6oA/exec',
   FRONTEND_URL: 'https://braianm-dev.github.io/aulagram/',
   RPC_TIMEOUT_MS: 35000,
